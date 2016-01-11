16127ce9b4222ca3fa11d395c5a38859b68c3fcb
Tushar Balar <balar.tushar1@gmail.com>
2016-01-12 02:20:33 +0530

    Set toggle button 	Improve gui design 	Hide toggle button and sidebar for next question

--- sansha/www/js/fetch-survey-details.js
+++ sansha/www/js/fetch-survey-details.js
@@ -1,38 +1,38 @@
 (function() {
-  
+
   var app = angular.module("myApp", []);
 
   app.controller('myCtrl', ['$scope', '$http', 'myService', '$timeout',
-    
+
     function ($scope, $http, myService, $timeout) {
-      
+
       var tmp = 0;
-      
+
       function findAndRemove(array, property, value) {
         array.forEach(function(result, index) {
           if(result[property] === value) {
             array.splice(index, 1);
             tmp = 1;
-          } 
+          }
         });
       }
 
       myService.getData(function(dataResponse) {
 
           $scope.surveys = dataResponse;
-          
+
           $scope.selections = [];
 
           var numInternalArrays = $scope.surveys.length;
           for (var i = 0; i < numInternalArrays; i++) {
             $scope.selections[i] = [];
           }
-          
+
           $scope.index = 0;
-          
+
           $scope.toggleSelectionCheckbox = function (QuestionId, value) {
             tmp = 0;
-            if (!value) { return; } 
+            if (!value) { return; }
             findAndRemove($scope.selections[$scope.index], 'categoryId', value.subCategoryId);
             if (tmp != 1) {
               $scope.selections[$scope.index].push({
@@ -44,22 +44,23 @@
               });
             }
           };
-          
+
           $scope.submitSelection = function() {
             $scope.value = $scope.selections[$scope.index];
             $scope.hideSubmitButton = true;
             $scope.disableCheckbox = true;
             $scope.hideEditButton = true;
             $("span").removeClass("subcategory-item");
+            $("ul li label span").css("color", "#636D7A");
           }
-          
+
       });
-      
+
       $scope.onlyRadioButtonGetQuestionId = function(QuestionId, a) {
         $scope.selections[$scope.index][$scope.inc].questionId = QuestionId.questionId;
         $scope.selections[$scope.index][$scope.inc].storeId = 1;
       }
-      
+
       $scope.pre = 0;
       $scope.array = [];
       $scope.i = 0;
@@ -71,45 +72,45 @@
         $scope.value = false;
         $("ul li label span").addClass("subcategory-item");
       }
-      
+
       $scope.inc = 0;
-      
+
       $scope.items = [];
-      
+
       $scope.next = function() {
         $scope.inc += 1;
       }
-      
+
       $scope.prev = function() {
         $scope.inc -= 1;
       }
 
       var prevFlag = 0;
-      
+
       $scope.previousQuestion = function() {
-        
+
         $scope.index -= 1;
         $scope.inc = 0;
         prevFlag = 1;
         // Check if subcategoryId is null or not
         angular.forEach($scope.surveys[$scope.index].category, function(value) {
-          
+
           angular.forEach(value.categoryItemDto, function(value1) {
-            
+
             if (value1.subCategoryId != undefined) {
-              
+
               $scope.hideSidebarItem = false;
               $scope.viewQuestionTrue = false;
               $scope.hideEditButton = true;
-            
+
             } else {
-              
+
               if ($scope.surveys[$scope.index] != undefined) {
                 if ($scope.surveys[$scope.index].category) {
                   $scope.Question = $scope.surveys[$scope.index].category;
                 }
               }
-              
+
               $scope.isQuestionTrue = [];
 
               angular.forEach($scope.Question, function(value) {
@@ -117,19 +118,19 @@
                   $scope.isQuestionTrue.push(value);
                 }
               });
-              
+
               $scope.showOnlyRadioButton = false;
               $scope.viewQuestionTrue = true;
             }
 
           });
-        });  
+        });
         if ($scope.surveys[$scope.index].questionType == "2") {
           $scope.noIndex = false;
           $scope.showOnlyRadioButton = true;
         }
       }
-      
+
       $scope.nextQuestion = function() {
         $scope.index += 1;
         $scope.inc = 0;
@@ -157,7 +158,7 @@
             $scope.isQuestionTrue.push(value);
           }
         });
-        
+
         if($scope.isQuestionTrue.length) {
 
           $scope.viewQuestionTrue = true;
@@ -179,7 +180,8 @@
             });
           }
           prevFlag = 0;
-          // $("#wrapper").toggleClass("toggled");
+          $("#wrapper").toggleClass("toggled");
+          $scope.toggleHide = true;
         } else {
            $scope.viewQuestionTrue = false;
           // $scope.hideSubmitButton = false;
